3742ca1ad1bf0571232773ff712597ed73fe06d0
GitRuii <145456302+Anish-Pun@users.noreply.github.com>
2025-11-13 15:02:14 +0100

    Add Requirements

--- Sudoku/cell.h
+++ Sudoku/cell.h
@@ -1,19 +1,20 @@
 #ifndef CELL_H
 #define CELL_H
 
-namespace sudoku {
+namespace sudoku { // C++ requirement: self-made namespace
 
-    class Cell
-    {
-    public:
-        Cell();
+class Cell           // Basis: useful class (represents one Sudoku cell)
+{
+public:
+    Cell();          // OOP: default constructor
 
-        int  value() const;
-        void setValue(int v);
+    int  value() const; // OOP: useful member function, const-correct
+    void setValue(int v); // OOP: useful setter for member variable
 
-    private:
-        int m_value;
-    };
-}
+private:
+    int m_value;     // OOP: encapsulation (private member variable)
+};
+
+} // namespace sudoku
 
 #endif // CELL_H
